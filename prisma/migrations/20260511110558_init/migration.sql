db786212c04a82982ceab23a5805fd3e97c6fa20
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-06-03 12:01:32 +0200

    Añadiendo migraciones

---
+++ prisma/migrations/20260511110558_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
